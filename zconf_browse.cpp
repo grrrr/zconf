0ad4f14b7c67d2b7e6d1aea5676931db182c0a81
Thomas Grill <gr@grrrr.org>
2007-01-03 18:13:49 +0000

    implemented proper support for txtrecords (bug #128)
    git-svn-id: https://svn.grrrr.org/ext/zconf/trunk@3533 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- zconf_browse.cpp
+++ zconf_browse.cpp
@@ -1,7 +1,7 @@
 /* 
 zconf - zeroconf networking objects
 
-Copyright (c)2006 Thomas Grill (gr@grrrr.org)
+Copyright (c)2006,2007 Thomas Grill (gr@grrrr.org)
 For information on usage and redistribution, and for a DISCLAIMER OF ALL
 WARRANTIES, see the file, "license.txt," in this distribution.  
 */
